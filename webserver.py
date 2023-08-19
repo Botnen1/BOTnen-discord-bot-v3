b86e2191be653e51b84ae5f826cd936eb5cca42b
Botnen <94934743+Botnen1@users.noreply.github.com>
2023-08-19 18:26:40 +0200

    webserver.py

---
+++ webserver.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+from threading import Thread
+
+app = Flask('')
+
+
+@app.route('/')
+
+def home():
+
+    return "I'm alive" + " Bot (BOTnen) is ONLINE" + " BOTnen is working as intended" + " Mail will be sent if e == event:Down_time"
+
+def run():
+
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():  
+
+  t = Thread(target=run)
+  t.start()
